c39405aad70779b823eb2282c3b364b482e4d147
robertschaap <robert.schaap@mac.com>
2017-12-13 17:00:26 +0100

    added responsive styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = props =>
   <header role="heading">
-    <h1>Final Project</h1>
+    <h1>Flash Cards</h1>
       <nav>
         <a href="/logout">Logout</a>
         <a href="/account">Account</a>
